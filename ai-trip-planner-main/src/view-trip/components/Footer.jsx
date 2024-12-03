03716c8aa340adc7c44358eab0e4f239c225765b
saisribhavana <bhavanathummala2003@gmaill.com>
2024-12-03 15:41:40 +0530

    Create Footer.jsx

---
+++ ai-trip-planner-main/src/view-trip/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='my-7'>
+        <h2 className='text-center text-black-800'>AI Travel Planner App</h2>
+    </div>
+  )
+}
+
+export default Footer
